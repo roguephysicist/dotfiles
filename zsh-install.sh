336c547b25705c1f9267da3fedf5373198f36453
Sean Anderson <sean.martin.anderson@gmail.com>
2013-12-07 20:59:17 -0600

    wrapping up mofo!

---
+++ zsh-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install newest version of zsh with homebrew
+brew install zsh
+
+# Starting zsh
+/bin/zsh
+
+# Cloning my prezto git repository to $HOME/.zprezto
+git clone --recursive https://github.com/roguephysicist/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Allowing for special character in filenames and symlinking into $HOME
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# Adding homebrew installed version to /etc/shells
+sudo printf "/usr/local/bin/zsh" >> /etc/shells
+
+# Changing default shell to new zsh installed by homebrew
+chsh -s /usr/local/bin/zsh
