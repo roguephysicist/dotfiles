89d59feaec03a640743c5d538f721f445dea1068
Sean Anderson <sean.martin.anderson@gmail.com>
2014-04-17 00:26:35 -0500

    done

--- symlink.sh
+++ symlink.sh
@@ -13,7 +13,7 @@ ln -sfh $PWD/symlinked/shell-functions $HOME/.functions
 ln -sfh $PWD/symlinked/shell-path $HOME/.path
 ln -sfh $PWD/symlinked/todotxt-todo.cfg $HOME/.todo.cfg
 
-if [ $shell == 'bash' ]; then
+if [ "$shell" == "bash" ]; then
     ln -sfh $PWD/symlinked/bash-aliases $HOME/.bash_aliases
     ln -sfh $PWD/symlinked/bash-exports $HOME/.bash_exports
     ln -sfh $PWD/symlinked/bash-profile $HOME/.bash_profile
