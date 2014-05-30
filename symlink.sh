6745ac6df019ff035d454d559105328f47db6b0d
Sean Anderson <sean.martin.anderson@gmail.com>
2014-05-30 08:12:59 -0500

    fixed -h option in symlink script

--- symlink.sh
+++ symlink.sh
@@ -3,20 +3,20 @@
 shell="$1"
 
 
-ln -sfh $PWD/symlinked/git-gitconfig $HOME/.gitconfig
-ln -sfh $PWD/symlinked/git-gitignore $HOME/.gitignore
-ln -sfh $PWD/symlinked/rsync-rsync_exclude $HOME/.rsync-exclude
-ln -sfh $PWD/symlinked/shell-aliases $HOME/.aliases
-ln -sfh $PWD/symlinked/shell-exports $HOME/.exports
-ln -sfh $PWD/symlinked/shell-extra $HOME/.extra
-ln -sfh $PWD/symlinked/shell-functions $HOME/.functions
-ln -sfh $PWD/symlinked/shell-path $HOME/.path
-ln -sfh $PWD/symlinked/todotxt-todo.cfg $HOME/.todo.cfg
+ln -sf $PWD/symlinked/git-gitconfig $HOME/.gitconfig
+ln -sf $PWD/symlinked/git-gitignore $HOME/.gitignore
+ln -sf $PWD/symlinked/rsync-rsync_exclude $HOME/.rsync-exclude
+ln -sf $PWD/symlinked/shell-aliases $HOME/.aliases
+ln -sf $PWD/symlinked/shell-exports $HOME/.exports
+ln -sf $PWD/symlinked/shell-extra $HOME/.extra
+ln -sf $PWD/symlinked/shell-functions $HOME/.functions
+ln -sf $PWD/symlinked/shell-path $HOME/.path
+ln -sf $PWD/symlinked/todotxt-todo.cfg $HOME/.todo.cfg
 
 if [ "$shell" == "bash" ]; then
-    ln -sfh $PWD/symlinked/bash-aliases $HOME/.bash_aliases
-    ln -sfh $PWD/symlinked/bash-exports $HOME/.bash_exports
-    ln -sfh $PWD/symlinked/bash-profile $HOME/.bash_profile
-    ln -sfh $PWD/symlinked/bash-prompt $HOME/.bash_prompt
-    ln -sfh $PWD/symlinked/bash-bashrc $HOME/.bashrc
+    ln -sf $PWD/symlinked/bash-aliases $HOME/.bash_aliases
+    ln -sf $PWD/symlinked/bash-exports $HOME/.bash_exports
+    ln -sf $PWD/symlinked/bash-profile $HOME/.bash_profile
+    ln -sf $PWD/symlinked/bash-prompt $HOME/.bash_prompt
+    ln -sf $PWD/symlinked/bash-bashrc $HOME/.bashrc
 fi
