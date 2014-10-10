ef4a110d4ac66e7ee27f9d07c485b83ff45b26be
Sean Anderson <sean.martin.anderson@gmail.com>
2014-10-09 21:48:42 -0500

    looking awesome!

---
+++ link/link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A very direct script for linking files into your Home directory.
+# Add files as needed.
+
+ln -sf $PWD/zshrc           $HOME/.zshrc
+ln -sf $PWD/gitconfig       $HOME/.gitconfig
+ln -sf $PWD/gitignore       $HOME/.gitignore
+ln -sf $PWD/todo.cfg        $HOME/.todo.cfg
+#ln -sf $PWD/bashrc          $HOME/.bashrc
+#ln -sf $PWD/rsync_exclude   $HOME/.rsync_exclude
--- symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# A very direct script for linking files into your Home directory.
-# Add files as needed.
-
-ln -sf $PWD/symlinked/zsh-zshrc $HOME/.zshrc
-ln -sf $PWD/symlinked/git-gitconfig $HOME/.gitconfig
-ln -sf $PWD/symlinked/git-gitignore $HOME/.gitignore
-ln -sf $PWD/symlinked/todotxt-todo.cfg $HOME/.todo.cfg
-ln -sf $PWD/symlinked/rsync-rsync_exclude $HOME/.rsync_exclude
