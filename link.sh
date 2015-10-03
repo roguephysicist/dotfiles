987ed90e6ae53977cf14b2f5db2dd18f0386eda5
Sean Anderson <sean.martin.anderson@gmail.com>
2015-10-02 21:40:46 -0500

    fixed

--- link.sh
+++ link.sh
@@ -2,11 +2,11 @@
 # A very direct script for linking files into your Home directory.
 # Add files as needed.
 
-ln -sf gitconfig       $HOME/.gitconfig
-ln -sf gitignore       $HOME/.gitignore
-ln -sf todo.cfg        $HOME/.todo.cfg
-ln -sf screenrc        $HOME/.screenrc
-ln -sf bashrc          $HOME/.bashrc
-ln -sf bash_profile    $HOME/.bash_profile
-ln -sf rsync_exclude   $HOME/.rsync_exclude
-#ln -sf zshrc           $HOME/.zshrc
+ln -sf $PWD/gitconfig       $HOME/.gitconfig
+ln -sf $PWD/gitignore       $HOME/.gitignore
+ln -sf $PWD/todo.cfg        $HOME/.todo.cfg
+ln -sf $PWD/screenrc        $HOME/.screenrc
+ln -sf $PWD/bashrc          $HOME/.bashrc
+ln -sf $PWD/bash_profile    $HOME/.bash_profile
+ln -sf $PWD/rsync_exclude   $HOME/.rsync_exclude
+#ln -sf $PWD/zshrc           $HOME/.zshrc
