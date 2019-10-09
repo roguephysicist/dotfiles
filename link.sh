cad9a30e707312622e273b9e2cdecb4d61c011be
Sean Anderson <sean.martin.anderson@gmail.com>
2019-10-08 21:44:04 -0500

    got rid of bashrc

--- link.sh
+++ link.sh
@@ -3,7 +3,6 @@
 # Add files as needed.
 
 ln -sf $PWD/bash_profile    $HOME/.bash_profile
-ln -sf $PWD/bashrc          $HOME/.bashrc
 ln -sf $PWD/gitconfig       $HOME/.gitconfig
 ln -sf $PWD/gitignore       $HOME/.gitignore
 ln -sf $PWD/screenrc        $HOME/.screenrc
